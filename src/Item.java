436e558a04dc9cb1f1df0e6a02efec6c4d621aea
quentinltg <quentint2005@gmail.com>
2026-09-18 15:57:25 +0200

    feat: add Item class

---
+++ src/Item.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Item {
+
+    private double prix;
+    private double tva;
+
+    public Item(){
+        this.prix = 0;
+        this.tva = 0;
+    }
+
+    public Item(double prix, double tva) {
+        this.prix = prix;
+        this.tva = tva;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+    public void setPrix(double prix) {}
+
+    public double getTva() {
+        return tva;
+    }
+
+    public void setTva(double tva) {
+        this.tva = tva;
+    }
+
+    public double getPrixTTC(){
+        return prix*(1+tva);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item(1,1);
+        System.out.println(item.getPrix());
+
+    }
+}
